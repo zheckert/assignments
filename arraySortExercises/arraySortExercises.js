f979cc6d48eb865dfd37f8e276fec8a6a8d753da
Zach Heckert <zach@zachheckert.com>
2020-09-07 12:18:30 -0600

    Completed an assignment

---
+++ arraySortExercises/arraySortExercises.js
@@ -0,0 +1,43 @@
+// function leastToGreatest(arr) {
+//     arr.sort((a, b) => a - b)
+//     return arr
+//   }
+  
+//   console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90] 
+
+//   function greatestToLeast(arr) {
+//     arr.sort((a, b) => b - a)
+//     return arr
+//   }
+  
+//   console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1] 
+
+  // function lengthSort(arr) {
+  //   arr.sort((a, b) => a.length - b.length)
+  //   return arr
+  // }
+  
+  // console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "wolf", "eaten", "family"] 
+
+  // function alphabetical(arr) {
+  //     arr.sort();
+  //     return arr;
+  // }
+  
+  // console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"])); // ["by", "dog", "eaten", "family", "wolf"] 
+
+  function byAge(arr){
+    arr.sort((a, b) => a.age - b.age)
+    return arr
+  }
+  
+  console.log(byAge([
+      { name: "Quiet Samurai", age: 22 },
+      { name: "Arrogant Ambassador", age: 100 },
+      { name: "Misunderstood Observer", age: 2 },
+      { name: "Unlucky Swami", age: 77 }
+  ]));
+  // => [ { name: 'Misunderstood Observer', age: 2 },
+  //  { name: 'Quiet Samurai', age: 22 },
+  //  { name: 'Unlucky Swami', age: 77 },
+  //  { name: 'Arrogant Ambassador', age: 100 } ]
---
+++ multipleArrayMethodsPart1/multipleArrayMethodsPart1.js
@@ -0,0 +1,53 @@
+function sortedOfAge(arr){
+    // Your code here
+ }
+ 
+ console.log(sortedOfAge(peopleArray));
+ 
+ /*
+ Output: 
+ [ 
+     "<li>Kyle Mooney is 27</li>",
+     "<li>Sarah Palin is 47</li>",
+     "<li>Rick Sanchez is 78</li>",
+     "<li>Morty Smith is 29</li>",
+     "<li>Lev Tolstoy is 82</li>" 
+ ]
+ */
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
