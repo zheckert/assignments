76df91f29e9dad12be2f996f82983cc88a683bce
Zach Heckert <zach@zachheckert.com>
2020-09-17 17:39:45 -0600

    Finished Princess. Started Pokemon.

---
+++ pokemonList/app.js
@@ -0,0 +1,27 @@
+//https://api.vschool.io/pokemon
+
+const xhr = new XMLHttpRequest()
+
+//I don't think it matters if this is asynchronous, but better safe than sorry.
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata.data)
+        pokeDump(data.results)
+    }
+}
+
+function pokeDump(arr){
+    for(let i = 0; i < arr.length; i++){
+        const p = document.createElement("p")
+        p.textContent = arr[i].name
+        document.body.appendChild(p)
+    }
+}
+
+//data.objects[0].pokemon
+
+//?
--- thePrincessIsInAnotherCastle/thePrincessIsInAnotherCastle.js
+++ thePrincessIsInAnotherCastle/thePrincessIsInAnotherCastle.js
@@ -1,28 +1,80 @@
+//This is my hero blueprint. 
 class Hero {
-    constructor(name, totalCoins, status = "small", hasStar = "false", gameActive = "true") {
-        this.name: ""
-        this.totalCoins = 0
+    constructor(totalCoins = 0, status = "Small", hasStar = false, gameActive = true){
+        this.name = ""
+        this.totalCoins = totalCoins
         this.status = status
         this.hasStar = hasStar
         this.gameActive = gameActive
     }
-    setName(namePicked){
-        TEST
+    setName(){
+        if(randomInteger() === 0){
+            this.name = "Mario"
+        } else {
+            this.name = "Luigi"
+        }
     }
     gotHit(){
-        if()
+        if(this.hasStar === true){
+            console.log("Your star protected you!")
+            this.hasStar = false;
+            return;
+        }
+        if(this.status === "Powered Up"){
+            this.status = "Big"
+            console.log("Oof! You took a hit and aren't powered up!")
+        }else if(this.status === "Big"){
+            this.status = "Small"
+            console.log("Yeeeouch! You took a big hit!")
+        }else if(this.status === "Small"){
+            this.status = "Dead"
+            console.log("You're dead!")
+            this.gameActive = false;
+        }
     }
     gotPowerup(){
-
+        if(this.status === "Small"){
+            this.status = "Big"
+            console.log("You ate a mushroom and now you're big!")
+        }else if(this.status === "Big"){
+            this.status = "Powered Up"
+            console.log("Now you're powered up!")
+        }else if(this.status === "Powered Up"){
+            console.log("You got a star!")
+            this.hasStar = true;
+            
+        }
     }
     addCoin(){
-
+        this.totalCoins++;
     }
     print(){
-
+        console.log(`Name: ${this.name}\nStatus: ${this.status}\nCoins: ${this.totalCoins}`)
     }
 }
 
-setTimeout(function(){});
+function randomInteger() {
+    return Math.round(Math.random());
+}
 
-new Hero
+const hero = new Hero()
+
+hero.setName();
+
+let running = setInterval(function(){
+    if(hero.gameActive !=false){
+        if(randomInteger() === 0){
+            hero.gotHit();
+        }else if(randomInteger() === 1){
+            hero.gotPowerup();
+        }else{
+            hero.addCoin();
+        }
+        hero.print();
+    }else{
+        clearInterval(running)
+    }
+}, 750);
+
+//TO DO
+//Make sure the global randomizer is available everywhere and that it works
