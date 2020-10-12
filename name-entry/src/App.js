7784d0a4245b167612aa9d1cb650c887e5b931b8
Zach Heckert <zach@zachheckert.com>
2020-10-12 14:18:57 -0600

    Finished name-entry assignment.

--- name-entry/src/App.js
+++ name-entry/src/App.js
@@ -1,23 +1,39 @@
+//To do- make sure that the names format correctly when added to the list.
+
+//Eric also says I'll need a map?
+
 import React from "react"
 
 class App extends React.Component {
+    
     constructor(){
+        
         super()
         this.state = {
             name: "",
-            pastNames: []
+            pastNames: [],
         }
     }
 
     handleChange = (event) => {
+        event.preventDefault();
         this.setState({
             name: event.target.value
         })
     }
 
     onSubmit = (event) => {
-        this.setState({
-            pastNames: event.target.value
+        event.preventDefault();
+        this.setState((prevState) => {
+            return { pastNames: [prevState.name, ...prevState.pastNames] }
+        })
+    }
+
+    display = () => {
+        return this.state.pastNames.map(name => {
+            return (
+                <h2>{name}</h2>
+            )
         })
     }
 
@@ -38,9 +54,7 @@ class App extends React.Component {
                 <h1>
                     {this.state.name}
                 </h1>
-                <h2>
-                    {this.state.pastNames}
-                </h2>
+                {this.display()}
             </div>
         )
     }
