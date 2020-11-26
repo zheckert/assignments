b4ee3e80853dc14bfd934720abb8929bc7a4d972
Zach Heckert <zach@zachheckert.com>
2020-11-25 17:01:31 -0700

    Having API issues. While I look for a new API I'm going to push what I have so far.

--- react-api-project/src/App.js
+++ react-api-project/src/App.js
@@ -6,10 +6,11 @@ export const App = () => {
   return(
     <div>
       <div className="center">
-        <h1>URL SHORTENER FOR YOUR PLAYSURE</h1>
+        <h1>URL SHORTENER</h1>
       </div>
       <div className="center">
-        Paste your link below and hit that button!
+      http://example.com/!leearn javascript$/
+      https://www.aliexpress.com/item/4000151357526.html?aff_platform=portals-tool&sk=_dShEGZD&aff_trace_key=125bdfedc61d458ea09ff0b304b93d49-1606344934772-02885-_dShEGZD&terminal_id=fe850e56997d42039baba5bd39d7616f&tmLog=new_Detail
       </div>
       <div className="center">
         <Shortener />
--- react-api-project/src/components/Shortener.js
+++ react-api-project/src/components/Shortener.js
@@ -1,19 +1,43 @@
-import Axios from "axios"
-import React from "react"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
 
-//procedure: take input and URL encode (encodeURI())
+//procedure: take input and URL encode (encodeURI()) https://attacomsian.com/blog/javascript-encode-url
 //send new URL to API
-//return and print new url somewhere
+//return and print new url somewhere https://goolnk.com/docs
 
 export const Shortener = () => {
-    Axios.get("")
+    const [ longLink, setLongLink ] = useState("")
+    const [ shortLink, setShortLink] = useState("")
+
+    // useEffect(() => {
+    //     axios.post("/api/v1/shorten", longLink)
+    //         .then((response) => {setShortLink(response.result_url)
+    //             console.log(response)})
+    //         .error((error) => console.log(error))
+    // }, [])
+    
+    const shorten = (e) => {
+        e.preventDefault();
+        axios.post("https://goolnk.com/api/v1/shorten", longLink)
+            .then((response) => {setShortLink(response.result_url)
+                console.log(response)})
+            .catch((error) => console.log(error))
+    }
+
+    const onChange = (e) => {
+        console.log(e.target.value)
+        setLongLink(encodeURI(e.target.value))
+        
+    }
+    console.log(longLink)
     return(
         <div>
             <div>
-                <input></input>
+                <input name="link" placeholder="Dump your link in here!" onChange={onChange}></input>
             </div>
+            {/* https://www.w3docs.com/snippets/css/how-to-change-input-and-button-images-with-css.html */}
             <div className="button">
-                <button>Shorten</button>
+                <button onClick={shorten}>Shorten</button>
             </div>
         </div>
     )
