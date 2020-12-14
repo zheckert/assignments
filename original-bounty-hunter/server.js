c96fcfad41df92a55c7060b5e2c5fe818d124b26
Zach Heckert <zach@zachheckert.com>
2020-12-14 16:49:55 -0700

    Began implementing Mongoose into project

--- original-bounty-hunter/server.js
+++ original-bounty-hunter/server.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
 
 app.use(express.json())
+app.use(morgan("dev"))
+
+mongoose.connect("mongodb://localhost:27017/bounty-database", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Successfully connected to the database")
+    )
 
 app.use("/bounty", require("./routes/bountyRouter"))
 
